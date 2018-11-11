1850f21d5a54a3f36ec065d391df3d9ad33e5d91
Horia Ion <horiapaulion@gmail.com>
2018-11-11 02:44:14 +0200

    Remove useless constructor

--- src/App.js
+++ src/App.js
@@ -7,11 +7,6 @@ import { GraphMenu } from './components/graphmenu';
 //Main page ----- Incorporates main menu, canvas ('map' as default display option), graph menu (for interacting with the canvas)
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-
-  }
-
   render() {
     return (
       <div style={{width: "100%", height: "100%", margin: "auto"}}>
